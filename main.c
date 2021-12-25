124f94c2c4eebabff4f6bc1a7bffa297bbd76131
Ahmet Tümiş <ahmettumis@gmail.com>
2021-12-25 16:14:52 +0300

    file operation done

--- main.c
+++ main.c
@@ -2,28 +2,77 @@
 #include <string.h>
 #include <stdlib.h>
 #include "omp.h"
+#include <sys/stat.h>
+
 
 #define MAX_SIZE 1000000
 
-void generate_array(){
-
-}
 
 int main(){
 
-    FILE *fp;
-    char buff[255];
-    int num;
+    int i = 0;
+    int buffer[2];
 
-    fp = fopen("girdi.txt", "r");
+    FILE *in_file = fopen("girdi.txt", "r");
 
-    if(fp == NULL){
-        printf("error!");
-        exit(1);
+    struct stat sb;
+    stat("girdi.txt", &sb);
+
+    char *file_contents = malloc(sb.st_size);
+    int counter = 0;
+
+    while (fscanf(in_file, "%[^\n] ", file_contents) != EOF && counter<2) {
+        printf("> %s\n", file_contents);
+        buffer[counter] = atoi(file_contents);
+        counter++;
+    }
+    
+    fclose(in_file);
+
+    for(i=0; i<2 ; i++){
+        printf("%d ", buffer[i]);
+    }
+    
+    
+    int *sortPtr;
+    sortPtr = (int*) malloc(buffer[1]* sizeof(int));
+    
+    if(sortPtr == NULL){  
+        printf("\nMemory could not be allocated...\n");
+        exit(0);
     }
 
-    
-    printf("anan");
+    else{ //arrayi doldur
+        printf("Memory allocated!\n");
+        
+        FILE *in_file = fopen("girdi.txt", "r");
 
+        struct stat sb;
+        stat("girdi.txt", &sb);
+
+        char *file_contents = malloc(sb.st_size);
+        int counter = 0;
+        int ptrCounter = 0;
+
+        while (fscanf(in_file, "%[^\n] ", file_contents) != EOF) {
+
+            if(counter>1){
+                printf("> %s\n", file_contents);
+                sortPtr[ptrCounter] = atoi(file_contents);
+                ptrCounter++;
+            }
+            counter++;
+        }
+        
+        fclose(in_file);
+
+        for (i = 0; i < buffer[1]; ++i) {
+            printf("%d ", sortPtr[i]);
+        }
+
+
+    }
+    
     return 0;
 }
+
